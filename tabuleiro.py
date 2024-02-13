08d5ae6de8b40d317cf1f56517b99f0236015c0f
Mateus Machado Costa <mateusdoug@hotmail.com>
2024-02-13 18:43:37 -0300

    tela de fim de jogo

--- tabuleiro.py
+++ tabuleiro.py
@@ -3,13 +3,17 @@ import pygame
 # INICIANDO PROGRAMAÇÃO DO DISPLAY
 
 # Resolução
-LARGURA = 800
-ALTURA = 525
+LARGURA = 1200
+ALTURA = 800
 
-display = pygame.display.set_mode((800, ALTURA))
+display = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Jogo Resta Um")
 pygame.font.init()
 clock = pygame.time.Clock()
+font = pygame.font.SysFont("arialblack", 40)
+fontFina = pygame.font.SysFont("calibri", 40)
+font_maior = pygame.font.SysFont("arialblack", 60)
+background = pygame.image.load("images/menu.jpg")
 
 # CORES
 PRETO = (0, 0, 0)
@@ -17,6 +21,16 @@ BRANCO = (255, 255, 255)
 COR_TABULEIRO = (204, 102, 0)
 COR_TABULEIRO_ESCURECIDO = (148, 74, 0)
 VERDE = (100, 225, 0)
+
+
+def desenhar_background(tela, image):
+    size = pygame.transform.scale(image, (1200, 800))
+    display.blit(image, (0, 0))
+
+
+def draw_text(tela, text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    display.blit(img, (x, y))
 
 
 class Jogo:
@@ -30,33 +44,35 @@ class Jogo:
             [-1, -1, 1, 1, 1, -1, -1],
             [-1, -1, 1, 1, 1, -1, -1],
         ]
-        self.turno = 0
+        self.seu_turno = True
         self.posicaoInicial = None
         self.posicaoFinal = None
-        self.vencedor = None
+        self.estado_atual = "Jogando"
 
     def avalia_posicao_clicada(self, coordenada):
         linha, coluna = linha_clicada(coordenada), coluna_clicada(coordenada)
-        print(f"{linha},{coluna}")
         if self.posicaoInicial == None:
-            self.posicaoInicial = [linha, coluna]
+            if self.tabuleiro[linha][coluna] == 1:
+                self.posicaoInicial = [linha, coluna]
         else:
             movimentos = self.posicoesPossiveis(
                 self.posicaoInicial[0], self.posicaoInicial[1]
             )
-            print(movimentos)
             if [linha, coluna] in movimentos:
-                print("entrou")
                 self.posicaoFinal = [linha, coluna]
                 self.moverPeca(self.posicaoInicial, self.posicaoFinal)
+                self.seu_turno = not self.seu_turno
                 self.posicaoInicial = None
                 self.posicaoFinal = None
             else:
-                self.posicaoInicial = [linha, coluna]
+                if self.tabuleiro[linha][coluna] == 1:
+                    self.posicaoInicial = [linha, coluna]
                 self.posicaoFinal = None
 
     def desenha_tabuleiro(self):
         matriz = []
+        tamanho_quadrado = round(ALTURA / 7)
+        raio_circulo = round(tamanho_quadrado / 3)
         for i in range(7):
             if i in [0, 1, 5, 6]:
                 matriz.append([0, 0, 1, 1, 1, 0, 0])
@@ -67,27 +83,59 @@ class Jogo:
             x = 0
             for c in range(len(matriz)):
                 if matriz[l][c] == 0:
-                    pygame.draw.rect(display, BRANCO, (x, y, 75, 75))
+                    pygame.draw.rect(
+                        display, BRANCO, (x, y, tamanho_quadrado, tamanho_quadrado)
+                    )
                 else:
-                    pygame.draw.rect(display, COR_TABULEIRO, (x, y, 75, 75))
+                    pygame.draw.rect(
+                        display,
+                        COR_TABULEIRO,
+                        (x, y, tamanho_quadrado, tamanho_quadrado),
+                    )
                 if self.posicaoInicial != None:
                     if [l, c] == [self.posicaoInicial[0], self.posicaoInicial[1]]:
                         pygame.draw.rect(
-                            display, COR_TABULEIRO_ESCURECIDO, (x, y, 75, 75)
+                            display,
+                            COR_TABULEIRO_ESCURECIDO,
+                            (x, y, tamanho_quadrado, tamanho_quadrado),
                         )
                     if [l, c] in self.posicoesPossiveis(
                         self.posicaoInicial[0], self.posicaoInicial[1]
                     ):
-                        pygame.draw.rect(display, VERDE, (x, y, 75, 75))
-                x += 75
-            y += 75
+                        pygame.draw.rect(
+                            display, VERDE, (x, y, tamanho_quadrado, tamanho_quadrado)
+                        )
+                x += tamanho_quadrado
+            y += tamanho_quadrado
 
         for l in range(len(self.tabuleiro)):
             for c in range(len(self.tabuleiro[l])):
                 x = ALTURA / 7 * c + ALTURA / 14
                 y = ALTURA / 7 * l + ALTURA / 14
                 if self.tabuleiro[l][c] == 1:
-                    pygame.draw.circle(display, BRANCO, (x, y), 20, 0)
+                    pygame.draw.circle(display, BRANCO, (x, y), raio_circulo, 0)
+
+    def desenha_menu(self):
+        if self.seu_turno:
+            jogada = "Você"
+        else:
+            jogada = "Seu oponente"
+        draw_text(
+            display,
+            f"Jogada:",
+            fontFina,
+            PRETO,
+            ALTURA,
+            0,
+        )
+        draw_text(
+            display,
+            jogada,
+            font,
+            COR_TABULEIRO,
+            ALTURA,
+            50,
+        )
 
     def posicoesPossiveis(self, X, Y):
         posicoesPossiveis = []
@@ -134,6 +182,20 @@ class Jogo:
         elif final[0] < inicial[0]:
             self.tabuleiro[final[0] + 1][final[1]] = 0
 
+    def verifica_fim_jogo(self):
+        somador = sum([contador.count(1) for contador in self.tabuleiro])
+        if somador == 1:
+            return "Fim"
+        if not self.existe_mov_possivel():
+            return "Empate"
+
+    def existe_mov_possivel(self):
+        for l in range(len(self.tabuleiro)):
+            for c in range(len(self.tabuleiro[l])):
+                if self.tabuleiro[l][c] == 1 and self.posicoesPossiveis(l, c) != []:
+                    return True
+        return False
+
 
 def coluna_clicada(pos):
     x = pos[0]
@@ -151,9 +213,67 @@ def linha_clicada(pos):
     return 6
 
 
+def tela_fim_jogo(jogo, fim):
+    sair = False
+    while not sair:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                sair = True
+                pygame.quit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+                    sair = True
+
+        display.fill(BRANCO)
+        desenhar_background(display, background)
+        draw_text(
+            display,
+            "Fim de jogo!",
+            font,
+            PRETO,
+            480,
+            300,
+        )
+        if fim == "Empate":
+            draw_text(
+                display,
+                "Empate",
+                font_maior,
+                COR_TABULEIRO,
+                480,
+                400,
+            )
+        elif fim == "Fim":
+            if jogo.seu_turno:
+                mensagem = "Você"
+                distancia = 400
+            else:
+                mensagem = "Seu oponente"
+                distancia = 250
+            draw_text(
+                display,
+                f"{mensagem} venceu!",
+                font_maior,
+                COR_TABULEIRO,
+                distancia,
+                400,
+            )
+        draw_text(
+            display,
+            "Aperte ESPAÇO para voltar para o início",
+            font,
+            PRETO,
+            180,
+            700,
+        )
+        pygame.display.update()
+        clock.tick(60)
+
+
 def loop_jogo():
     jogo = Jogo()
     sair = False
+    print(pygame.font.get_fonts())
     while not sair:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
@@ -161,8 +281,16 @@ def loop_jogo():
                 pygame.quit()
             if evento.type == pygame.MOUSEBUTTONDOWN:
                 jogo.avalia_posicao_clicada(pygame.mouse.get_pos())
-        display.fill(PRETO)
+
+        display.fill(BRANCO)
         jogo.desenha_tabuleiro()
+        jogo.desenha_menu()
+
+        fim_jogo = jogo.verifica_fim_jogo()
+        if fim_jogo is not None:
+            sair = True
+            tela_fim_jogo(jogo, fim_jogo)
+
         pygame.display.update()
         clock.tick(60)
 
