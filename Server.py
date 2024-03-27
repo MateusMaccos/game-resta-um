03cfa5c7284b91fa0a4a130acb36526d6e23f6c6
Mateus Machado Costa <75508372+MateusMaccos@users.noreply.github.com>
2024-03-27 17:16:26 -0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -15,12 +15,103 @@ tabuleiroInicial = [
 class Jogo(object):
     def __init__(self):
         self.tabuleiro = [linha[:] for linha in tabuleiroInicial]
+        # Tabuleiro para testar vencer
+        # self.tabuleiro = [
+        #     [-1, -1, 0, 0, 0, -1, -1],
+        #     [-1, -1, 0, 0, 0, -1, -1],
+        #     [1, 1, 0, 0, 0, 0, 0],
+        #     [0, 0, 0, 0, 0, 1, 1],
+        #     [0, 0, 0, 1, 1, 0, 0],
+        #     [-1, -1, 0, 0, 0, -1, -1],
+        #     [-1, -1, 0, 0, 0, -1, -1],
+        # ]
+        self.jogadores = []
+        self.estado_atual = "Jogando"
+        self.turno_atual = None
+        global textos
+        textos = []
+    
+    def pegar_estado_atual(self):
+        return self.estado_atual
+
+    def pegar_turno(self):
+        return self.turno_atual
+    
+    def mudar_estado_atual(self,valor):
+        self.estado_atual = valor
+    
     def pegar_tabuleiro(self):
         return self.tabuleiro
-    def jogar(self,posicaoX,posicaoY):
-        x = int(posicaoX)
-        y = int(posicaoY)
-        self.tabuleiro[x][y]=1
+
+    def resetar_jogo_atual(self):
+        self.tabuleiro = [linha[:] for linha in tabuleiroInicial]
+        self.estado_atual = "Jogando"
+        textos.clear()
+
+    def registrar_jogador(self, id):
+        if(len(self.jogadores)==[]):
+            self.turno_atual=id
+        self.jogadores.append(id)
+
+    def posicoesPossiveis(self, X, Y):
+        posicoesPossiveis = []
+        if (
+            X + 1 != 7
+            and self.tabuleiro[X + 1][Y] not in [0, -1]
+            and X + 2 != 7
+            and self.tabuleiro[X + 2][Y] == 0
+        ):
+            posicoesPossiveis.append([X + 2, Y])
+        if (
+            X - 1 != -1
+            and self.tabuleiro[X - 1][Y] not in [0, -1]
+            and X - 2 != -1
+            and self.tabuleiro[X - 2][Y] == 0
+        ):
+            posicoesPossiveis.append([X - 2, Y])
+        if (
+            Y + 1 != 7
+            and self.tabuleiro[X][Y + 1] not in [0, -1]
+            and Y + 2 != 7
+            and self.tabuleiro[X][Y + 2] == 0
+        ):
+            posicoesPossiveis.append([X, Y + 2])
+        if (
+            Y - 1 != -1
+            and self.tabuleiro[X][Y - 1] not in [0, -1]
+            and Y - 2 != -1
+            and self.tabuleiro[X][Y - 2] == 0
+        ):
+            posicoesPossiveis.append([X, Y - 2])
+
+        return posicoesPossiveis
+
+    def moverPeca(self, inicial, final):
+        self.tabuleiro[final[0]][final[1]] = 1
+        self.tabuleiro[inicial[0]][inicial[1]] = 0
+        if final[1] > inicial[1]:
+            self.tabuleiro[final[0]][final[1] - 1] = 0
+        elif final[1] < inicial[1]:
+            self.tabuleiro[final[0]][final[1] + 1] = 0
+        elif final[0] > inicial[0]:
+            self.tabuleiro[final[0] - 1][final[1]] = 0
+        elif final[0] < inicial[0]:
+            self.tabuleiro[final[0] + 1][final[1]] = 0
+        self.seu_turno = not self.seu_turno
+
+    def verifica_fim_jogo(self):
+        somador = sum([contador.count(1) for contador in self.tabuleiro])
+        if somador == 1:
+            self.estado_atual = "Fim"
+        elif not self.existe_mov_possivel():
+            self.estado_atual = "Empate"
+
+    def existe_mov_possivel(self):
+        for l in range(len(self.tabuleiro)):
+            for c in range(len(self.tabuleiro[l])):
+                if self.tabuleiro[l][c] == 1 and self.posicoesPossiveis(l, c) != []:
+                    return True
+        return False
 
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
